24181a02519aaf451f024073b39f6068d3d8dedc
luca020400 <luca.stefani.ge1@gmail.com>
2015-03-28 19:28:01 +0100

    titan: get rid of charger & charger_res
    Change-Id: Ia70991095894da23eeaf1b3a90b0fe8ab7403f7b

--- omni_titan.mk
+++ omni_titan.mk
@@ -26,11 +26,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
-# Charger
-PRODUCT_PACKAGES += \
-    charger_res_images \
-    charger
-
 PRODUCT_NAME := omni_titan
 PRODUCT_DEVICE := titan
 PRODUCT_BRAND := Motorola
